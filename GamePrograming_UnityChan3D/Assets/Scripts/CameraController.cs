e71fbce245649c8d835a4ddf83bf269325997f52
kanzakiSho5 <syo5.4545@gmail.com>
2019-07-15 07:20:43 +0900

    Scopeモードの仮追加

--- GamePrograming_UnityChan3D/Assets/Scripts/CameraController.cs
+++ GamePrograming_UnityChan3D/Assets/Scripts/CameraController.cs
@@ -14,8 +14,13 @@ public class CameraController : MonoBehaviour {
     [Header("Camera")]
     [SerializeField]
     private GameObject ADSRightPos;
+    [SerializeField]
+    private GameObject ScopeCam;
+
 	private float yRot;
 	private float xRot;
+
+    private int wepon = 0;
 
 	private void Start()
 	{
@@ -25,23 +30,38 @@ public class CameraController : MonoBehaviour {
 
 	private void Update()
 	{
+        print(Input.GetAxis("Mouse ScrollWheel"));
+        wepon += (int)(Input.GetAxis("Mouse ScrollWheel") * 10);
 
         if (Time.timeScale <= 0) return;
         if (Input.GetMouseButton(1))
         {
-            ADSRightPos.SetActive(true);
+            
             this.yRot += Input.GetAxisRaw("Mouse X");
             this.xRot += Input.GetAxisRaw("Mouse Y");
             xRot = Mathf.Clamp(xRot, -LimitRotateY, LimitRotateY);
 
             this.transform.position = this.LookPoint.position;
 
-            ADSRightPos.transform.localEulerAngles = new Vector3(-this.xRot, 0, 0);
-            ADSRightPos.transform.parent.localEulerAngles = new Vector3(0, this.yRot, 0);
+            if(wepon < 0)
+            {
+                ADSRightPos.SetActive(true);
+                ScopeCam.SetActive(false);
+                ADSRightPos.transform.localEulerAngles = new Vector3(-this.xRot, 0, 0);
+                ADSRightPos.transform.parent.localEulerAngles = new Vector3(0, this.yRot, 0);
+            }
+            else
+            {
+                ScopeCam.SetActive(true);
+                ADSRightPos.SetActive(false);
+                ScopeCam.transform.localEulerAngles = new Vector3(-this.xRot, 0, 0);
+                ScopeCam.transform.parent.localEulerAngles = new Vector3(0, this.yRot, 0);
+            }
             return;
         }
 
         ADSRightPos.SetActive(false);
+        ScopeCam.SetActive(false);
 
 		if(this.LookPoint)
 		{
