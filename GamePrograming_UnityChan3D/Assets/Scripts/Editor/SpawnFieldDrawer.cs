fcac28c421ddd4f9c9b6cdf34e17bc6c7ac0f39a
kanzakiSho5 <syo5.4545@gmail.com>
2019-07-15 04:23:16 +0900

    Assetの追加(metaファイルのみ)

---
+++ GamePrograming_UnityChan3D/Assets/Scripts/Editor/SpawnFieldDrawer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+//[CustomPropertyDrawer(typeof(SpawnField))]
+public class SpawnFieldDrawer :  PropertyDrawer{
+
+    private SpawnField spawnField;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        using(new EditorGUI.PropertyScope(position, label, property))
+        {
+            // ラベルの大きさを小さくする
+            //EditorGUIUtility.labelWidth = 50;
+
+            position.height = EditorGUIUtility.singleLineHeight;
+
+            var halfWidth = position.width * 0.5f;
+
+            // それぞれのRectを求める
+            var centerRect = new Rect(position)
+            {
+                width = position.width,
+                height = 64,
+            };
+
+            var sizeRect = new Rect(position)
+            {
+                height = EditorGUIUtility.singleLineHeight,
+                y = centerRect.y + EditorGUIUtility.singleLineHeight + 2
+            };
+
+            // 各プロパティーのGUIを求める
+            var centerProperty = property.FindPropertyRelative("center");
+            var sizeProperty = property.FindPropertyRelative("size");
+
+            // 各プロパティーを描画
+            EditorGUI.Vector3Field(centerRect, centerProperty.displayName, centerProperty.vector3Value);
+            EditorGUI.Vector3Field(sizeRect, sizeProperty.displayName, sizeProperty.vector3Value);
+
+        }
+    }
+    
+
+}
---
+++ GamePrograming_UnityChan3D/Assets/Scripts/Editor/SpawnPointEditor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+//[CustomEditor(typeof(SpawnPoint))]
+public class SpawnPointEditor : Editor
+{
+    /* TODO: Vector3含む自作クラスのReorderableListの値の変更の適応
+    ReorderableList spawnFieldList;
+    
+
+    private void OnEnable()
+    {
+         
+        var spawnFieldProp = serializedObject.FindProperty("spawnFields");
+        
+
+        
+        spawnFieldList = new ReorderableList(serializedObject, spawnFieldProp,true, true, true, true);
+        spawnFieldList.elementHeight = 50;
+        spawnFieldList.drawElementCallback = (rect, index, isActive, isFocused) =>
+        {
+            var element = spawnFieldProp.GetArrayElementAtIndex(index);
+            rect.height -= 4;
+            rect.y += 2;
+            EditorGUI.PropertyField(rect, element);
+        };
+
+        spawnFieldList.onCanRemoveCallback = (list) =>
+        {
+            return list.count > 1;
+        };
+
+        var defaultColor = GUI.backgroundColor;
+
+        spawnFieldList.drawHeaderCallback = (rect) => EditorGUI.LabelField(rect, spawnFieldProp.displayName);
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        if (spawnFieldList.index >= spawnFieldList.count)
+            spawnFieldList.index = spawnFieldList.count - 1;
+        
+        /*
+        SpawnPoint myTarget = (SpawnPoint)target;
+        DrawPropertiesExcluding(serializedObject, new string[] { "spawnFields" });
+        
+
+        EditorGUI.BeginChangeCheck();
+        spawnFieldList.DoLayoutList();
+        if (EditorGUI.EndChangeCheck())
+            Debug.Log(serializedObject.ApplyModifiedProperties());
+        
+        this.DrawDefaultInspector();
+        
+    }
+    */
+}
+
+
