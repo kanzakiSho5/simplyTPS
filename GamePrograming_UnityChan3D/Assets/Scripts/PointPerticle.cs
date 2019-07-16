5d08c93bf78db67fe71b204fb85f0b18b570aa42
kanzakiSho5 <syo5.4545@gmail.com>
2019-07-17 02:14:22 +0900

    敵を倒したときに獲得した点数が出るように

---
+++ GamePrograming_UnityChan3D/Assets/Scripts/PointPerticle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointPerticle : MonoBehaviour {
+
+    private Text text;
+    private void OnEnable()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.up;
+        text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - 0.01f);
+        if (text.color.a <= 0)
+            Destroy(gameObject);
+    }
+
+    public void GetPoint(int point)
+    {
+        text.text = "+ " + point;
+    }
+
+}
